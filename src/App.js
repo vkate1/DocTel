9f11ff66292ab8011f86e57edfcec41a311557d4
swapnil <swapnilkole7500@gmail.com>
2022-07-23 11:13:24 +0530

    formatted text-3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
-import Main from './Components/MainComponents';
-import {BrowserRouter} from 'react-router-dom';
+import Main from "./Components/MainComponents";
+import { BrowserRouter } from "react-router-dom";
 class App extends Component {
-
   render() {
     return (
       <BrowserRouter>
-      <div className="App">
-        <Main />
-      </div>
+        <div className="App">
+          <Main />
+        </div>
       </BrowserRouter>
     );
   }
